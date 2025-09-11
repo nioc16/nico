8df0345e757ce007f9b8fdb361d5dadca0351426
nioc16 <nicolas.guill1604@gmail.com>
2025-09-11 13:18:37 +0000

    ok

--- js/script1.js
+++ js/script1.js
@@ -15,9 +15,25 @@ let nonDefini;
 console.log(texte, nombre, estVrai, vide, nonDefini);
 
 let a = 10;
-let b = 10;
+let b = 5;
 
 console.log("Addition :", a + b);
 console.log("Soustraction :", a - b);
 console.log("Multiplication :", a * b);
 console.log("Division :", a / b);
+
+    // Saisie de la note
+   let note = 20;
+
+        if (note >= 16 && note <= 20) {
+            console.log("Très bien");
+        } else if (note >= 12 && note <= 15) {
+            console.log("Bien");
+        } else if (note >= 10 && note <= 11) {
+            console.log("Passable");
+        } else if (note >= 0 && note < 10) {
+            console.log("Échec");
+        } else {
+            console.log("Note invalide, veuillez saisir un nombre entre 0 et 20.");
+        }
+
