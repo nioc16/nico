77b41fd0740bd0e5f789bf249d307fb6db2ae8e3
nioc16 <nicolas.guill1604@gmail.com>
2025-09-11 10:28:19 +0000

    ok

--- js/script1.js
+++ js/script1.js
@@ -1,8 +1,15 @@
-let nom = "Alice";
-let age = 25;
+let nom = "Nicolas";
+let age = 28;
 let estEtudiant = true;
 
 console.log("Nom :", nom);
 console.log("Âge :", age);
 console.log("Est étudiant :", estEtudiant);
 
+let texte = "Bonjour";
+let nombre = 42;
+let estVrai = true;
+let vide = null;
+let nonDefini;
+
+console.log(texte, nombre, estVrai, vide, nonDefini);
